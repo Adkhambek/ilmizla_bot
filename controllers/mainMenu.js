39a6acd3a57d105d1548995fd1de0de4f0c97ca1
Adham <muzaffarov.adham@gmail.com>
2022-01-08 09:32:00 +0500

    public feedback ended

--- controllers/mainMenu.js
+++ controllers/mainMenu.js
@@ -4,10 +4,10 @@ const { menu } = require("../configs/texts");
 const model = require("../models/users");
 const { admins } = require("../configs/keys");
 
-module.exports = async (context) => {
+module.exports = async (context, text = menu) => {
     const chatId = context.chat.id;
     await model.defaultPage(chatId);
     const checkAdmin = admins.find((el) => el === chatId);
     const keyboard = !checkAdmin ? menuBtns : adminMenu;
-    context.reply(menu, Markup.keyboard(keyboard).resize());
+    context.reply(text, Markup.keyboard(keyboard).resize());
 };
--- models/feedbacks.js
+++ models/feedbacks.js
@@ -1,8 +1,14 @@
 const orm = require("../utils/orm");
 
 exports.insertFeedback = (data) => orm.insertOne("feedbacks", data);
-exports.getFeedback = (chat_id) =>
-    orm.selectOne(
+exports.sendFeedback = (chat_id) =>
+    orm.updateOne(
         "feedbacks",
-        `chat_id = ${chat_id} AND date = (SELECT MAX(date) FROM feedbacks)`
+        { status: 1 },
+        `chat_id = ${chat_id} AND status = 0`
     );
+exports.deleteFeedback = (chat_id) =>
+    orm.deleteOne("feedbacks", `chat_id = ${chat_id} AND status = 0`);
+exports.getFeedbacks = () => orm.selectMany("feedbacks", `status = 1`);
+exports.getAcceptedFeedbacks = (chat_id) =>
+    orm.selectOne("feedbacks", `chat_id = ${chat_id} AND status = 0`);
